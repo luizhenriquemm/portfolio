69b4f0a19ef1e31522f7c4adbeb445b186b58b8c
Luiz Henrique <68759905+luizhenriquemm@users.noreply.github.com>
2021-04-22 15:33:28 -0300

    Delete Python + Django/siteloja directory

--- Python + Django/siteloja/loja/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Vendedores, Clientes, Vendas, ItensVendas
-
-# Register your models here.
-
-admin.site.register(Vendedores)
-admin.site.register(Clientes)
-admin.site.register(Vendas)
-admin.site.register(ItensVendas)
